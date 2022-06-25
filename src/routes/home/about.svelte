f7328783dbea606d7a6c7d24a35c135282cc2bf8
carbon <cheeseburgerbbgun@gmail.com>
2022-06-25 12:47:59 -0600

    Added basic who am I to about page

--- src/routes/home/about.svelte
+++ src/routes/home/about.svelte
@@ -1,3 +1,40 @@
 <sevlte:head>
 	<title>About</title>
 </sevlte:head>
+
+<div class="container">
+	<div class="intro">
+		<article class="description">
+			<h3>Who am I?</h3>
+			<p>
+				I'm Brandon, a software engineer that is currently focusing on full-stack web development.
+				The tech I work with is either Sveltekit + prima w/ sql or MERN (MongoDB, Express, React,
+				Node).
+			</p>
+		</article>
+		<img src="" alt="" />
+	</div>
+</div>
+
+<style lang="scss">
+	.container {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.intro {
+		display: flex;
+		flex-wrap: wrap-reverse;
+	}
+
+	.description {
+		h3 {
+			font-size: 3rem;
+		}
+		p {
+		  font-size: 2rem;
+    }
+	}
+
+</style>
