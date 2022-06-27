ac6101204053f7b08bcdb28e1ff553400465a04f
carbon <cheeseburgerbbgun@gmail.com>
2022-06-27 14:57:40 -0600

    Fixed about container not letting starfall go all the way down.

--- src/routes/home/__layout.svelte
+++ src/routes/home/__layout.svelte
@@ -7,15 +7,15 @@
 <Header />
 <slot />
 
-<div class="container">
+<div class="rainContainer">
 	<Rain minHeight={100} maxHeight={500} rainCounter={4} />
 </div>
 
 <style>
-	.container {
+	.rainContainer {
 		position: absolute;
 		width: 100%;
-		height: 100%;
+    height: 100%;
 		top: 0;
 		left: 0;
 		z-index: -1;
